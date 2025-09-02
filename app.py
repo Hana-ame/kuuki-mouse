c70aff25d53dc8992734fa5ed19bb171f11d6a11
lumino <luminovoez@gmail.com>
2025-09-02 19:55:24 +0800

    添加输入

--- app.py
+++ app.py
@@ -109,11 +109,14 @@ def get_data(x: float, y: float, z: float, alpha: float, beta: float, gamma: flo
     app.update_mouse()
 
 
-def get_message(message: str):
-    # print(f"message{message}")
+def mouse_event(message: str):
     app.click_mouse(message)
 
+def text_event(message:str):
+    app.type_text(message)
 
+def key_event(message:str):
+    app.press_key(message)
 # if __name__ == "__main__":
 #     try:
 #         asyncio.run(main())
